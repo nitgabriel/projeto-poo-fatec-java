2849a4c0acb31806228b3ec6ea0e07312527f1fc
nitgabriel <gabrield114@gmail.com>
2024-05-23 14:27:05 -0300

    Alteração: Criação do menu principal, faltando ainda configurar o controller.

--- src/main/java/br/com/fatec/App.java
+++ src/main/java/br/com/fatec/App.java
@@ -7,6 +7,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import javafx.scene.image.Image;
 
 /**
  * JavaFX App
@@ -17,8 +18,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("view/Menu"), 930, 505);
         stage.setScene(scene);
+        stage.getIcons().add(new Image("/br/com/fatec/images/corgi.png"));
+        stage.setTitle("ULTRAPETS");
+        stage.setResizable(false);
         stage.show();
     }
 
---
+++ src/main/java/br/com/fatec/controller/MenuController.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package br.com.fatec.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * FXML Controller class
+ *
+ * @author Gabriel
+ */
+public class MenuController implements Initializable {
+
+    @FXML
+    private AnchorPane main;
+    @FXML
+    private Button btnGerenciarPets;
+    @FXML
+    private Button btnGerenciarDonos;
+    @FXML
+    private Button btnGerenciarVeterinarios;
+    @FXML
+    private Button btnGerenciarAgendamentos;
+    @FXML
+    private Button btnGerenciarMedicamentos;
+    @FXML
+    private Button btnGerenciarUnidades;
+    @FXML
+    private Button btnConsultarInfo;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        
+        
+    }    
+
+    @FXML
+    private void btnGerenciarPets_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGerenciarDonos_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGerenciarVeterinarios_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGerenciarAgendamentos_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGerenciarMedicamentos_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnGerenciarUnidades_Click(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnConsultarInfo_Click(ActionEvent event) {
+    }
+    
+}
